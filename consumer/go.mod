fad54dc11b136e5072971c04b0dca224b87ec40f
Fahad Hossain <fahad.hossain@virtualminds.com>
2023-03-17 10:17:29 +0100

    feat: add example code

---
+++ consumer/go.mod
@@ -0,0 +1,5 @@
+module github.com/fa7ad/kafka-test-consumer
+
+go 1.16
+
+require github.com/Shopify/sarama v1.38.1
---
+++ consumer/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/Shopify/sarama"
+)
+
+var brokers = []string{
+	os.Getenv("KAFKA_BROKER"),
+}
+
+func main() {
+	config := sarama.NewConfig()
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest //get offset for the oldest message on the topic
+	config.Consumer.Offsets.Retry.Max = 3
+	config.Consumer.Return.Errors = true
+
+	consumer, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		log.Fatal("Could not create consumer: ", err.Error())
+	}
+
+	log.Println("start consuming")
+	subscribe("important", consumer)
+
+	// just for blocking the function
+	c := make(chan struct{})
+	<-c
+}
+
+func subscribe(topic string, consumer sarama.Consumer) {
+	partitionList, err := consumer.Partitions(topic) //get all partitions on the given topic
+	if err != nil {
+		log.Fatal("Error retrieving partitionList ", err.Error())
+	}
+	for _, partition := range partitionList {
+		pc, err := consumer.ConsumePartition(topic, partition, sarama.OffsetOldest)
+		go func() {
+			if err != nil {
+				log.Printf("could not create partition consumer, err: %s", err.Error())
+			}
+
+			for {
+				select {
+				case err := <-pc.Errors():
+					log.Printf("could not process message, err: %s", err.Error())
+				case message := <-pc.Messages():
+					log.Printf("received message %v\n", string(message.Value))
+				}
+			}
+		}()
+	}
+}
